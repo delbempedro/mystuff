58d8a5d22e2f006a014349abc5de3cc7fa09eff2
delbempedro <pedrodelbemo@gmail.com>
2024-04-25 17:15:53 -0300

    change some methods

--- errorcalculator.py
+++ errorcalculator.py
@@ -7,89 +7,86 @@ class medida:
         self._value = value
         self._error = abs(error)
 
-    def __add__(self, plus):
+    def __add__(self, other):
 
-        if not isinstance(plus, medida):
-            if not isinstance(plus, float or int):
-                raise TypeError("Should be medida")
-            else:
-                return medida(self._value+plus, self._error)
+        if isinstance(other, medida):
+            return medida(self._value+other._value, self._error+other._error)
+        elif isinstance(other, float or int):
+            return medida(self._value+other, self._error)
         else:
-            return medida(self._value+plus._value, self._error+plus._error)
+            raise TypeError("Should be medida")
         
-    def __radd__(self, plus):
+    def __radd__(self, other):
 
-        if not isinstance(plus, medida):
-            if not isinstance(plus, float or int):
-                raise TypeError("Should be medida")
-            else:
-                return medida(self._value+plus, self._error)
+        if isinstance(other, medida):
+            return medida(self._value+other._value, self._error+other._error)
+        elif isinstance(other, float or int):
+            return medida(self._value+other, self._error)
         else:
-            return medida(self._value+plus._value, self._error+plus._error)
+            raise TypeError("Should be medida")
         
-    def __sub__(self, subtract):
+    def __sub__(self, other):
 
-        if not isinstance(subtract, medida):
-            if not isinstance(subtract, float or int):
-                raise TypeError("Should be medida")
-            else:
-                return medida(self._value-subtract, self._error)
+        if isinstance(other, medida):
+            return medida(self._value-other._value, self._error+other._error)
+        elif isinstance(other, float or int):
+            return medida(self._value-other, self._error)
         else:
-            return medida(self._value-subtract._value, self._error+subtract._error)
+            raise TypeError("Should be medida")
         
-    def __rsub__(self, subtract):
+    def __rsub__(self, other):
 
-        if not isinstance(subtract, medida):
-            if not isinstance(subtract, float or int):
-                raise TypeError("Should be medida")
-            else:
-                return medida(self._value-subtract, self._error)
+        if isinstance(other, medida):
+            return medida(other._value-self._value, self._error+other._error)
+        elif isinstance(other, float or int):
+            return medida(other-self._value, self._error)
         else:
-            return medida(subtract._value-self._value, self._error+subtract._error)
+            raise TypeError("Should be medida")
+            
     
-    def __mul__(self, mult):
+    def __mul__(self, other):
 
-        if not isinstance(mult, medida):
-            if not isinstance(mult, float or int):
+        if not isinstance(other, medida):
+            if not isinstance(other, float or int):
                 raise TypeError("Should be medida")
             else:
-                return medida(self._value*mult, self._error*mult)
+                return medida(self._value*other, self._error*other)
         else:
-            return medida(self._value*mult._value, self._value*mult._error+self._error+mult._value)
+            return medida(self._value*other._value, self._value*other._error+self._error+other._value)
         
-    def __rmul__(self, mult):
+    def __rmul__(self, other):
 
-        if not isinstance(mult, medida):
-            if not isinstance(mult, float or int):
+        if not isinstance(other, medida):
+            if not isinstance(other, float or int):
                 raise TypeError("Should be medida")
             else:
-                return medida(self._value*mult, self._error*mult)
+                return medida(self._value*other, self._error*other)
         else:
-            return medida(self._value*mult._value, self._value*mult._error+self._error+mult._value)
+            return medida(self._value*other._value, self._value*other._error+self._error+other._value)
         
-    def __truediv__(self, divi):
+    def __truediv__(self, other):
 
-        if not isinstance(divi, medida):
-            if not isinstance(divi, float or int):
+        if not isinstance(other, medida):
+            if not isinstance(other, float or int):
                 raise TypeError("Should be medida")
             else:
-                return medida(self._value/divi, self._error/divi)
+                return medida(self._value/other, self._error/other)
         else:
-            a = self._value*divi._error-self._error+divi._value
-            b = divi._value*divi._value
-            return medida(self._value/divi._value, a/b)
+            a = self._value*other._error-self._error+other._value
+            b = other._value*other._value
+            return medida(self._value/other._value, a/b)
     
-    def __rtruediv__(self, divi):
+    def __rtruediv__(self, other):
 
-        if not isinstance(divi, medida):
-            if not isinstance(divi, float or int):
+        if not isinstance(other, medida):
+            if not isinstance(other, float or int):
                 raise TypeError("Should be medida")
             else:
-                return medida(self._value/divi, -divi*self._error/self._value**2.0)
+                return medida(self._value/other, -other*self._error/self._value**2.0)
         else:
-            a = self._error+divi._value-self._value*divi._error
+            a = self._error+other._value-self._value*other._error
             b = self._value*self._value
-            return medida(divi._value/self._value, a/b)
+            return medida(other._value/self._value, a/b)
         
     def __repr__(self):
 
@@ -101,13 +98,6 @@ class medida:
             raise TypeError("Should be medida")
         else:
             return equal._value-(self._error+equal._error)<self._value<equal._value+(self._error+equal._error)
-
-    def __pow__(self, num):
-
-        if not isinstance(num, float or int):
-            raise TypeError("Should be a number")
-        else:
-            return medida(self._value**num, num*self._value**(num-1)*self._error)
         
     def __neg__(self):
 
@@ -120,7 +110,17 @@ class medida:
     def ln(self):
 
         return medida(math.log(self._value), self._error/self._value)
+    
+    def __pow__(self, other):
+
+        if isinstance(other, medida):
+            return medida(self._value**other, other*self._value**(other-1)*self._error)
+        elif isinstance(other, float or int):
+            return medida(self._value**other, other*self._value**(other-1)*self._error)
+        else:
+            raise TypeError("Should be a medida")
+            
 
 a = medida(20,1)
 b = medida(19,1)
-print((a+b)/(a-b)*b**2.0)
+print(a==b)
